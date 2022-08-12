f6962037ef59eba6b70fb04fee750267d20dd513
Ryan Chiechi <rchiech@ncsu.edu>
2022-08-12 11:47:44 -0400

    stage class test

--- stage/esp302.py
+++ stage/esp302.py
@@ -36,15 +36,15 @@ class ESP302:
         if self.error:
             raise Exception()  # TODO: throw a real exception
 
-    __cmd(self, axis, cmd, param=None):
+    def __cmd(self, axis, cmd, param=None):
         if param is None:
-            _cmdstr = b"%d%s;%dTS\r"%(axis,cmd,axis)
+            _cmdstr = "%d%s;%dTS\r"%(axis,cmd,axis)
         else:
-            _cmdstr = b"%d%s%d;%dTS\r"%(axis,cmd,param,axis)
+            _cmdstr = "%d%s%d;%dTS\r"%(axis,cmd,param,axis)
         self.dev.write(_cmdstr)
         return asciitobinary(self.dev.read())
     
-    __moveindefinitely(self, axis, direction):
+    def __moveindefinitely(self, axis, direction):
         _status = asciitobinary(__cmd(axis, 'MF', direction))
         while not bool(int(_status[2]))
             if __bittobool(_status[8]) or __bittobool(_status[9]):
@@ -52,29 +52,40 @@ class ESP302:
             time.sleep(1)
         return True
    
-   __bittobool(self, bit):
+   def __bittobool(self, bit):
         return bool(int(bit))
+        
+    def cleanup(self):
+        for axis in (1,2,3):
+        if not motorOff(axis):
+            self.error = True
+        if self.error:
+            raise Exception()  # TODO: throw a real exception
    
-    motorOn(self, axis):
+    def motorOn(self, axis):
         "Turn on axis motor."
         _status = asciitobinary(__cmd(axis, 'MO'))
         return __bittobool(_status[1])
 
-    motorOff(self, axis):
+    def motorOff(self, axis):
         "Turn off axis motor."
         _status = asciitobinary(__cmd(axis, 'MF'))
         return __bittobool(_status[1])
     
-    stop(self, axis):
+    def stop(self, axis):
         _status = asciitobinary(__cmd(axis, 'ST'))
         return __bittobool(_status[2])
     
-    moveMax(self, axis):
+    def moveMax(self, axis):
         return __moveindefinitely(axis, '+')
 
-    moveMax(self, axis):
+    def moveMax(self, axis):
         return __moveindefinitely(axis, '-')
 
     
-
+if __name__ == '__main__':
+    from stage.backend import Telnet
+    telnet = Telnet()
+    stage = ESP302(telnet)
+    stage.cleanup()
         
