81d74cd3ed8f87a9188ddac35a97b267bdb0b120
Ryan Chiechi <rchiech@ncsu.edu>
2023-07-06 09:13:20 -0400

    Enumerate all serial ports on non-posix OSes

--- meas/util.py
+++ meas/util.py
@@ -1,17 +1,47 @@
 import os
-import platform
+import sys
+import glob
+import serial
 
 def enumerateDevices(_first=None):
     _filter = ''
-    if platform.system() == "Darwin":
+    if sys.platform.startswith("darwin"):
         _filter = 'usbmodem'
-    if platform.system() == "Linux":
+    if sys.platform.startswith("linux"):
         _filter = 'ttyACM'
     if _first is not None:
         _devs = [_first]
     else:
         _devs = []
-    for _dev in os.listdir('/dev'):
-        if _filter.lower() in _dev.lower():
-            _devs.append(_dev)
+    try:
+        for _dev in os.listdir('/dev'):
+            if _filter.lower() in _dev.lower():
+                _devs.append(os.path.join('/', 'dev', _dev))
+    except FileNotFoundError:
+        _devs = serial_ports()
     return _devs
+
+def serial_ports(_first=None):
+    """
+    Enumerates all serial devices on the system.
+
+    Returns:
+    A list of serial device paths.
+    """
+    _ports = [_first] if _first is not None else []
+    if os.name == "nt":
+        _ports += [f"COM{i}" for i in range(256)]
+    elif os.name == "posix":
+        _ports += glob.glob("/dev/tty[A-Za-z]*")
+    else:
+        raise EnvironmentError("Unsupported platform")
+
+    ports = []
+    for _port in _ports:
+        try:
+            s = serial.Serial(_port)
+            s.close()
+            ports.append(_port)
+        except (OSError, serial.SerialException):
+            pass
+    return ports
--- thermo/util.py
+++ thermo/util.py
@@ -19,39 +19,35 @@ def enumerateDevices(_first=None):
     try:
         for _dev in os.listdir('/dev'):
             if _filter.lower() in _dev.lower():
-                _devs.append(_dev)
+                _devs.append(os.path.join('/', 'dev', _dev))
     except FileNotFoundError:
         _devs = serial_ports()
     return _devs
 
-def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
+def serial_ports(_first=None):
     """
-    # https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
+    Enumerates all serial devices on the system.
 
-    result = []
-    for port in ports:
+    Returns:
+    A list of serial device paths.
+    """
+    _ports = [_first] if _first is not None else []
+    if os.name == "nt":
+        _ports += [f"COM{i}" for i in range(256)]
+    elif os.name == "posix":
+        _ports += glob.glob("/dev/tty[A-Za-z]*")
+    else:
+        raise EnvironmentError("Unsupported platform")
+
+    ports = []
+    for _port in _ports:
         try:
-            s = serial.Serial(port)
+            s = serial.Serial(_port)
             s.close()
-            result.append(port)
+            ports.append(_port)
         except (OSError, serial.SerialException):
             pass
-    return result
+    return ports
 
 def init_thermo_device(device):
     print(f"\nInitializing {device}...", end='')
