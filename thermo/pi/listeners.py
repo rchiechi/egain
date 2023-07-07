b810247de2eaaaba15116f9dc1c9edfc983c26e6
Ryan Chiechi <rchiech@ncsu.edu>
2023-07-07 10:34:47 -0400

    Trailing __

--- thermo/pi/listeners.py
+++ thermo/pi/listeners.py
@@ -8,7 +8,7 @@ class Thermo(Netcontroller):
     last_rt = 0.0
     last_v = 0.0
 
-    def _post_init__(self):
+    def _post_init(self):
         self.lt = self.devices[0]['left']
         self.rt = self.devices[0]['right']
         self.voltmeter = self.devices[1]
@@ -31,48 +31,6 @@ class Thermo(Netcontroller):
     def voltage(self):
         return self.last_v
 
-#     def _listener_main(self):
-#         """
-#         The main application loop
-#         """
-#         print(f"Starting listener in {self.addr}")
-#         while self.alive.is_set() and self.command == tc.COMMAND_RUN:
-#             # block until a client connection is recieved
-#             with self.listener.accept() as conn:
-# 
-#                 # receive the subscription request from the client
-#                 message = conn.recv()
-# 
-#                 # if it's a shut down command, return to stop this thread
-#                 if isinstance(message, str) and message == tc.COMMAND_STOP:
-#                     print(f"Listener {self.addr} dying.")
-#                     self.alive.clear()
-#                     self.message = tc.COMMAND_STOP
-#                     return
-#                 if isinstance(message, str) and message == tc.COMMAND_READ:
-#                     self.__update()
-#                     conn.send({'left': self.last_lt,
-#                                'right': self.last_rt,
-#                                'voltage': self.last_v})
-#                 if isinstance(message, str) and message == tc.COMMAND_STAT:
-#                     if self.lt is not None and self.rt is not None:
-#                         _status = tc.STAT_OK
-#                     else:
-#                         _status = tc.STAT_ERROR
-#                     conn.send({'status': _status})
-# 
-#     def _updater_main(self):
-#         print("Starting 5 second updater")
-#         start_time = time.time()
-#         while self.alive.is_set() and self.command == tc.COMMAND_RUN:
-#             if time.time() - start_time > 5:
-#                 self.__update()
-#                 start_time = time.time()
-#             time.sleep(0.1)
-#         print("Updater thread dying.")
-#         self.alive.clear()
-#         self.message = tc.COMMAND_STOP
-
     def _update(self):
         if self.initialized:
             try:
@@ -90,50 +48,3 @@ class Thermo(Netcontroller):
             except ValueError as msg:
                 print(f"Error reading voltage: {msg}")
                 self.last_json['voltage'] = 0.0
-
-
-
-# class volta(threading.Thread):
-
-#     last_v = 0.0
-
-#     def __init__(self, alive, smu={'voltmeter':None}, **kwargs):
-#         super().__init__()
-#         self.alive = alive
-#         self.addr = (kwargs.get('address', '0.0.0.0'), kwargs.get('port', VOLTA_PORT))
-#         self.authkey = kwargs.get('authkey', b'1234')
-#         self.lt = thermocouples['left']
-#         self.rt = thermocouples['right']
-
-#     def run(self):
-#         print(f"Starting listener in {self.addr}")
-#         listener = Listener(self.addr, authkey=self.authkey)
-#         while self.alive.is_set():
-#             with listener.accept() as conn:
-#                 msg = conn.recv()
-#                 if msg == 'close':
-#                     conn.close()
-#                     self.kill()
-#                     break
-#                 if msg == 'read':
-#                     self.sendtemps(conn)
-#         listener.close()
-
-#     def kill(self):
-#         print(f"{self.name} dying.")
-#         self.alive.clear()
-
-#     def sendvoltage(self, conn):
-#         return
-
-#     @property
-#     def voltage(self):
-#         return self.last_v
-
-
-# if __name__ == '__main__':
-#     alive = threading.Event()
-#     alive.set()
-#     thermocomm = Thermo(alive)
-#     # voltacomm = volta(alive)
-#     alive.clear()
