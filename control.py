75e67c081834ab3172a5e0568c177498290966af
Ryan Chiechi <rchiech@ncsu.edu>
2025-03-26 08:44:04 -0400

    ?

--- control.py
+++ control.py
@@ -86,9 +86,8 @@ class seebeckstats(curses_updater):
         for _dev in enumerateDevices(first='serial0'):
             if _dev in DEVSINUSE.values():
                 continue
-            print("1")
+            sys.exit()
             voltmeter = K2182A(_dev)
-            print("2")
             if voltmeter.initialize(auto_sense_range=True):
                 DEVSINUSE['seebeckstats'] = _dev
                 break
